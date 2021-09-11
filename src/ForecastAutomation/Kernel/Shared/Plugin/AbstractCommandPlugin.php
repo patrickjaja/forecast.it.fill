81ec06d290be8179cebf44ddba8aa0b01eda2feb
patrickjaja <patrickjaja@users.noreply.github.com>
2021-09-11 23:09:35 +0000

    Autofix php-cs sniffs

--- src/ForecastAutomation/Kernel/Shared/Plugin/AbstractCommandPlugin.php
+++ src/ForecastAutomation/Kernel/Shared/Plugin/AbstractCommandPlugin.php
@@ -2,12 +2,7 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of forecast.it.fill.
- * (c) Patrick Jaja <patrickjaja@web.de>
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+// This file is part of forecast.it.fill project. (c) Patrick Jaja <patrickjaja@web.de> This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
 
 namespace ForecastAutomation\Kernel\Shared\Plugin;
 
