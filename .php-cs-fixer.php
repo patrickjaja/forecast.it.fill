4d063b45841a16020ef5f75995884b84d52275a2
nxs_schoenfeld <patrickjaja@web.de>
2021-09-12 01:05:59 +0200

    fixer update

---
+++ .php-cs-fixer.php
@@ -0,0 +1,32 @@
+<?php
+
+$header = <<<'EOF'
+This file is part of forecast.it.fill.
+(c) Patrick Jaja <patrickjaja@web.de>
+This source file is subject to the MIT license that is bundled
+with this source code in the file LICENSE.
+EOF;
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__.'/src',__DIR__.'/tests']);
+
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setRules(
+        [
+            '@PHP71Migration' => true,
+            '@PHP71Migration:risky' => true,
+            '@PHPUnit75Migration:risky' => true,
+            '@PhpCsFixer' => true,
+            '@PhpCsFixer:risky' => true,
+            'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
+            'header_comment' => ['header' => $header],
+            '@PHP80Migration' => true,
+            '@PHP80Migration:risky' => true,
+            'heredoc_indentation' => false,
+        ]
+    )
+    ->setFinder($finder);
+
+return $config;
