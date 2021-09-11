1f61b397696c19f0410ca43f343ab80ee95cd726
patrickjaja <patrickjaja@users.noreply.github.com>
2021-09-11 23:12:25 +0000

    Autofix php-cs sniffs

--- src/ForecastAutomation/ForecastDataImport/Shared/Plugin/ForecastImportActivityConsoleCommandPlugin.php
+++ src/ForecastAutomation/ForecastDataImport/Shared/Plugin/ForecastImportActivityConsoleCommandPlugin.php
@@ -2,7 +2,12 @@
 
 declare(strict_types=1);
 
-// This file is part of forecast.it.fill project. (c) Patrick Jaja <patrickjaja@web.de> This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+/*
+ * This file is part of forecast.it.fill project.
+ * (c) Patrick Jaja <patrickjaja@web.de>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace ForecastAutomation\ForecastDataImport\Shared\Plugin;
 
