d2b6d31d66d0862fced258749ae700eb2630353c
patrickjaja <patrickjaja@users.noreply.github.com>
2021-09-23 23:01:03 +0000

    Autofix php-cs sniffs

--- src/ForecastAutomation/MattermostClient/Business/MattermostApi.php
+++ src/ForecastAutomation/MattermostClient/Business/MattermostApi.php
@@ -53,7 +53,8 @@ class MattermostApi
     public function getPosts(MattermostPostsQueryDto $postsQueryDto): PromiseInterface
     {
         $this->auth();
-        $wrapPromise = new Promise(function () use ($postsQueryDto, &$wrapPromise) {
+
+        return new Promise(function () use ($postsQueryDto, &$wrapPromise) {
             $res = $this->guzzleClient->requestAsync(
                 'GET',
                 sprintf(self::POSTS_API, $postsQueryDto->channelId),
@@ -68,7 +69,6 @@ class MattermostApi
 
             $wrapPromise->resolve(json_decode((string) $res->getBody(), true, 512, JSON_THROW_ON_ERROR)['posts']);
         });
-        return $wrapPromise;
     }
 
     private function auth(): string
