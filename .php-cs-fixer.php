52fbc3325f4e03bb3cdc53dbf6aeb0160ea2de2c
nxs_schoenfeld <patrickjaja@web.de>
2021-09-24 01:09:01 +0200

    forecast.it.fill-12 return promises on activity plugins

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,35 +15,35 @@ $config
     ->setRiskyAllowed(true)
     ->setRules(
         [ // https://mlocati.github.io/php-cs-fixer-configurator/#version:3.1|fixer:php_unit_internal_class
-            '@PHP71Migration' => true,
-            '@PhpCsFixer' => true,
-            'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
-            'header_comment' => ['header' => $header],
-            '@PHP80Migration' => true,
-            'heredoc_indentation' => false,
-            '@PSR2' => true,
-            'array_syntax' => ['syntax' => 'short'],
-            'ordered_imports' => ['sort_algorithm' => 'alpha'],
-            'no_unused_imports' => false,
-            'not_operator_with_successor_space' => true,
-            'trailing_comma_in_multiline' => true,
-            'phpdoc_scalar' => true,
-            'unary_operator_spaces' => true,
-            'binary_operator_spaces' => true,
-            'blank_line_before_statement' => [
-                'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
-            ],
-            'phpdoc_single_line_var_spacing' => true,
-            'phpdoc_var_without_name' => true,
-            'class_attributes_separation' => [
-                'elements' => [
-                    'method' => 'one',
-                ],
-            ],
-            'method_argument_space' => [
-                'on_multiline' => 'ensure_fully_multiline',
-                'keep_multiple_spaces_after_comma' => true,
-            ],
+//            '@PHP71Migration' => true,
+//            '@PhpCsFixer' => true,
+//            'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
+//            'header_comment' => ['header' => $header],
+//            '@PHP80Migration' => true,
+//            'heredoc_indentation' => false,
+//            '@PSR2' => true,
+//            'array_syntax' => ['syntax' => 'short'],
+//            'ordered_imports' => ['sort_algorithm' => 'alpha'],
+//            'no_unused_imports' => false,
+//            'not_operator_with_successor_space' => true,
+//            'trailing_comma_in_multiline' => true,
+//            'phpdoc_scalar' => true,
+//            'unary_operator_spaces' => true,
+//            'binary_operator_spaces' => true,
+//            'blank_line_before_statement' => [
+//                'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+//            ],
+//            'phpdoc_single_line_var_spacing' => true,
+//            'phpdoc_var_without_name' => true,
+//            'class_attributes_separation' => [
+//                'elements' => [
+//                    'method' => 'one',
+//                ],
+//            ],
+//            'method_argument_space' => [
+//                'on_multiline' => 'ensure_fully_multiline',
+//                'keep_multiple_spaces_after_comma' => true,
+//            ],
             'single_trait_insert_per_statement' => true,
         ]
     )
